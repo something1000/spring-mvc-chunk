4abffbbb6fb30903dc98e0a36660e356c3eb8cdc
Bartłomiej <something1112@o2.pl>
2018-02-22 23:39:44 +0100

    starter-project

---
+++ src/main/java/com/gawrych/readinglist/HomeController.java
@@ -0,0 +1,16 @@
+package com.gawrych.readinglist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+
+}
